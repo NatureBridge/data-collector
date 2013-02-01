1b74839ca32b29d296349c37f5d8800545ddbc85
k3mm3tt <kemmett@naturebridge.org>
2013-02-01 14:49:20 -0800

    NBLog Archive and Restorte

--- NatureBridge/NBLog.m
+++ NatureBridge/NBLog.m
@@ -14,99 +14,106 @@
 static NSMutableArray* logTbl;  // Table of Transmit Logs
 static UITextView *textView;    // TextView Object to display log.
 static NSString* logFile;       // File to Archive Transmit Logs
-
-// Save TextView Field to display log Data
--(void) start:(UITextView *)logView {
-    textView = logView;
-}
-
+//
+// CREATE A NEW LOG
+//
 // Create Log, add DateTimestamp to log name, and display it.
--(void) create:(NSString *)name {
-    logName = [NSString stringWithFormat:@"%@ %@",name,[self getDateTime]];
+-(void) create:(NSString *)name  in:(UITextView *)textFld
+{   logName = [NSString stringWithFormat:@"%@ %@",name,[NBLog getDateTime]];
+    NSLog(@"NBLog: create: %@",logName);
+    if (textView == nil)
+        textView = textFld;
+    else return; // Log is busy !
     logText = [[NSMutableString alloc] initWithCapacity:1000];
     if ([logTbl count] >= logMax) {
-        [logTbl removeObjectAtIndex:logMax-1];
-    }
+        [logTbl removeObjectAtIndex:logMax-1];  }
     [logTbl insertObject:self atIndex:0];
     [self add:logName];
 }
-
 // Add header to log and scroll to bottopm
 -(void) header:(NSString *)text {
     [self add:[NSString stringWithFormat:@"\n%@",text]];
 }
-
 // Add Error text to log
 -(void) error:(NSString *)text {
     [self add:[NSString stringWithFormat:@"\tError: %@",text]];
 }
-
 // Add Response text to log
 -(void) response:(NSString *)text {
     [self add:[NSString stringWithFormat:@"\tResponse: %@",text]];
 }
-
 // Add text to log and scroll to bottopm
 -(void) add:(NSString *)text {
     [logText appendFormat:@"%@\n",text];
     textView.text = logText;
     [textView scrollRangeToVisible:NSMakeRange([textView.text length], 0)];
 }
-
-// Respond to List Button Click - List Logs in Popup Action Sheet
-- (void)listLogs:(UIView *)view
-{
-    UIActionSheet *actionSheet = [[UIActionSheet alloc]
-                                  initWithTitle:nil delegate:self cancelButtonTitle:nil
-                                  destructiveButtonTitle:nil otherButtonTitles:nil];
-    for (NBLog *log in logTbl) {
-        [actionSheet addButtonWithTitle:log.logName];
-    }
-    [actionSheet showInView:view];
+// Close log
+-(void) close
+{   NSLog(@"NBLog: close.");
+    textView = nil;
 }
-
-// Respond to Log List Action Sheet Button Click Display Log
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
-{
-    NBLog *log = [logTbl objectAtIndex:buttonIndex];
-    textView.text = log.logText;
-}
-
 // getDateTime in format: yyyy-MM-dd HH:mm:ss
-- (NSString *) getDateTime {
++ (NSString *) getDateTime {
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"YYYY-MM-DD HH:mm:SS"];
+    [dateFormatter setDateFormat:@"YYYY-MM-dd HH:mm:SS"];
     return [dateFormatter stringFromDate:[NSDate date]];
 }
-
-// Get FilenName for Logs in Arcghive
-+(void) getFileName{
-    NSString *cacheDir =
-    [NSSearchPathForDirectoriesInDomains(NSCachesDirectory,
-                                         NSUserDomainMask, YES) objectAtIndex:0];
-    logFile = [cacheDir stringByAppendingPathComponent:logFileName];
+//
+// REVIEW PAST LOGS
+//
+// Respond to List Button Click - List Logs in Popup Action Sheet
+-(void)listLogs:(UIView *)view in:(UITextView *)textFld
+{   //NSLog(@"NBLog: listLogs.");
+    if (textView == nil)
+        textView = textFld;
+    else return; // Log is busy !
+    UIActionSheet *actionSheet = [[UIActionSheet alloc]
+        initWithTitle:nil delegate:self cancelButtonTitle:nil
+        destructiveButtonTitle:nil otherButtonTitles:nil];
+    for (NBLog *log in logTbl) {
+        [actionSheet addButtonWithTitle:log.logName]; }
+    [actionSheet showInView:view];
 }
-
-// Save Logs to Archive
+// Respond to Log List Action Sheet Button Click Display Log
+- (void)actionSheet:(UIActionSheet *)actionSheet
+        clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if ((buttonIndex >=0) && (buttonIndex < [logTbl count])) {
+        NBLog *log = [logTbl objectAtIndex:buttonIndex];
+        textView.text = log.logText; }
+    textView = nil;
+}
+//
+// PERSIST LOGS
+//
+// Save Logs to Archive - Called from AppDelegate Launch
 +(void) archive{
+    if (logFile == nil) [self getFileName];
+    NSLog(@"NBLog: archive: %@",logFileName);
+    //[NBLog dump];
     [NSKeyedArchiver archiveRootObject:logTbl toFile:logFile];
 }
-
-// Restore Logs from Archives
+// Restore Logs from Archives- Called from AppDelegate Terminate
 +(void) restore{
     if (logFile == nil) [self getFileName];
+    NSLog(@"NBLog: restore: %@",logFileName);
     if (logTbl == nil)
         logTbl = [NSKeyedUnarchiver unarchiveObjectWithFile:logFile];
     if (logTbl == nil)
         logTbl = [[NSMutableArray alloc] initWithCapacity:logMax];
+    //[NBLog dump];
 }
-
+// Dump Logs Diagnostic
++(void) dump {
+    for (int i=0; i<[logTbl count]; i++) {
+        NBLog *log = (NBLog *)[logTbl objectAtIndex:i];
+        NSLog(@"NBLog: Log: %@",log.logText); } 
+}
 // Encode Log for Archiving
 -(void) encodeWithCoder:(NSCoder *)encoder{
     [encoder encodeObject:self.logName forKey:@"Name"];
     [encoder encodeObject:self.logText forKey:@"Text"];
 }
-
 // Decode Log from Archive
 -(id)initWithCoder:(NSCoder *)decoder{
     if (self = [super init]) {
@@ -114,5 +121,12 @@ static NSString* logFile;       // File to Archive Transmit Logs
         self.logText = [decoder decodeObjectForKey:@"Text"];
     }
     return(self);
+}
+// Get FilenName for Archive
++(void) getFileName{
+    NSString *cacheDir =
+    [NSSearchPathForDirectoriesInDomains(NSCachesDirectory,
+         NSUserDomainMask, YES) objectAtIndex:0];
+    logFile = [cacheDir stringByAppendingPathComponent:logFileName];
 }
 @end
