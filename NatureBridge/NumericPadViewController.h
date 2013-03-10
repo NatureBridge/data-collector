5e5c22954248714469345f91965072408961cf7d
Alex Volkovitsky <volki@fastmail.fm>
2013-03-10 15:35:33 -0700

    [#42535281] Update license to OSL 3.0
    Story: https://www.pivotaltracker.com/story/show/42535281

--- NatureBridge/NumericPadViewController.h
+++ NatureBridge/NumericPadViewController.h
@@ -2,9 +2,11 @@
 //  NumericPadViewController.h
 //  NatureBridge
 //
-//  Created by Richard F Emmett on 1/23/13.
-//  Copyright (c) 2013 Alex Volkovitsky. All rights reserved.
+//  Permission is granted to copy, distribute and/or modify this file under the
+//  terms of the Open Software License v. 3.0 (OSL-3.0). You may obtain a copy of
+//  the license at http://opensource.org/licenses/OSL-3.0
 //
+
 #import <UIKit/UIKit.h>
 
 @interface NumericPadViewController : UIViewController {
--- NatureBridge/TransmitViewController.m
+++ NatureBridge/TransmitViewController.m
@@ -2,8 +2,11 @@
 //  TransmitViewController.m
 //  NatureBridge
 //
-//  Created by Alex Volkovitsky on 1/5/13.
-//  Copyright (c) 2013 Alex Volkovitsky. All rights reserved.
+//  Copyright 2013 NatureBridge. All Rights Reserved.
+//
+//  Permission is granted to copy, distribute and/or modify this file under the
+//  terms of the Open Software License v. 3.0 (OSL-3.0). You may obtain a copy of
+//  the license at http://opensource.org/licenses/OSL-3.0
 //
 
 #import "TransmitViewController.h"
